8ae3c63c29214246d709b0771093488e06542f48
Charbel SONON <charbelsnn@gmail.com>
2024-11-21 23:26:47 +0100

    build

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,7 +288,7 @@ const handlePlantsClick = (e) => {
             <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
           </svg>
           {/* Afficher le nombre d'articles dans le panier à l'intérieur de l'icône */}
-          {totalItems > 0 && (
+          { (
             <span style={{
               position: 'absolute',
               top: '25%', 
